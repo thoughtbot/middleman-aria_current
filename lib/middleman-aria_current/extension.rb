72197d3943eaf31d6abc6fd10dc029003624b095
Corey Ward <corey.atx@gmail.com>
2018-03-19 19:08:59 -0500

    Trim trailing slash from `current_path`
    This resolves issues where linking to `/about` fails to match
    the `current_page.url` for a directory index (`/about/`)

--- lib/middleman-aria_current/extension.rb
+++ lib/middleman-aria_current/extension.rb
@@ -4,7 +4,7 @@ class AriaCurrent < ::Middleman::Extension
   FILE_EXTENSION = /\.(\w+)$/
 
   helpers do
-    def current_link_to(*arguments, aria_current: "page", **options, &block)
+    def current_link_to(*arguments, aria_current: "page", debug: false, **link_options, &block)
       if block_given?
         text = capture(&block)
         path = arguments[0]
@@ -13,14 +13,14 @@ class AriaCurrent < ::Middleman::Extension
         path = arguments[1]
       end
 
-      link_options = options
-      current_path = current_page.url.to_s.gsub(FILE_EXTENSION, "")
+      current_path = current_page.url.to_s.
+        gsub(FILE_EXTENSION, "").
+        chomp("/")
 
-      if current_path == path
-        link_options.merge!("aria-current" => aria_current)
-      end
+      link_options["aria-current"] = aria_current if current_path == path
+      link_options["data-current-path"] = current_path if debug
 
-      link_to(text, path, link_options)
+      link_to text, path, link_options
     end
   end
 end
